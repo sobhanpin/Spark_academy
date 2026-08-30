2fc6d861389fe3f55b3e6a0d7eef613a73323f1d
sobhanpin <sobhanpin13@gmail.com>
2026-08-30 11:55:36 +0330

    Remove file extensions from imports in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
-import { AuthProvider } from './contexts/AuthContext.tsx'
+import App from './App'
+import { AuthProvider } from './contexts/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
